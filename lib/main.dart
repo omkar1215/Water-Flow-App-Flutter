3165b274eb51e0cd67756963bee7e1bc4e8bd1bc
Omkar Ganachari <60459028+omkar1215@users.noreply.github.com>
2023-02-05 22:02:09 +0530

    Final code 2

--- lib/main.dart
+++ lib/main.dart
@@ -6,72 +6,41 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget{
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MyHomePage(),
-    );
-  }
+  _MyAppState createState() => _MyAppState();
 }
 
-var flag = 1;
-var switchFlag = "ON";
-var num=800.0;
+bool click = true;
 
-class MyHomePage extends StatelessWidget {
+class _MyAppState extends State<MyApp> {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            WaveWidget(
-              config: CustomConfig(
-                  gradients: [
-                    [Colors.lightBlue, Colors.lightBlueAccent],
-                    [Colors.lightBlueAccent, Colors.blue],
-                    [Colors.blueAccent, Colors.lightBlueAccent],
-                    [Colors.lightBlueAccent, Colors.blue],
-                  ],
-                  gradientBegin: Alignment.centerLeft,
-                  gradientEnd: Alignment.centerRight,
-                  durations: [3500, 19440, 10800, 6000],
-                  heightPercentages: [0.20, 0.23, 0.25, 0.30]),
-              size: Size(num, num),
-            ),Container(
-              width: 500.0,
-              height: 500.0,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  RaisedButton(
-                    child: Text(switchFlag, style: TextStyle(fontSize: 20),),
-                    onPressed: (){
-                      if(flag==1){
-                        flag=0;
-                        switchFlag="ON";
-                        num=0.0;
-                      }else{
-                        flag=1;
-                        switchFlag="OFF";
-                        num=800.0;
-                      }
-                    },
-                    color: Colors.indigoAccent,
-                    textColor: Colors.white,
-                    padding: EdgeInsets.all(8.0),
-                    splashColor: Colors.grey,
-                  )
-                ],
-              ),
-            )
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Scaffold(
+        body: GestureDetector(
+          onTap:((){
 
-          ],
+            setState(() {
+              click=!click;
+            });
+          }), // Image tapped
+          child: Image.asset( (click==false) ?
+            'assets/wateroff.png' : 'assets/wateron.png',
+//fit: BoxFit.fill, // Fixes border issues
+            width: 300.0,
+            height: 300.0,
+          ),
         ),
       ),
     );
   }
 }
+
+
+
+
+//
+
+
+
